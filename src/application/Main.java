e217caa5faa258ee332ba2da8cf1b036e4b231aa
Stein <miguel.wainst@gmail.com>
2026-06-03 18:13:27 -0300

    Criando classe Department

---
+++ src/application/Main.java
@@ -0,0 +1,11 @@
+package application;
+
+import model.Department;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Department department = new Department(1, "Books");
+
+    }
+}
