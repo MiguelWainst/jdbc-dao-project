0c263fe4e1bb735628066844026680c1a7940d3b
Stein <miguel.wainst@gmail.com>
2026-06-03 18:37:22 -0300

    Criando a entidade seller

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,11 +1,17 @@
 package application;
 
 import model.Department;
+import model.Seller;
+
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
 
         Department department = new Department(1, "Books");
 
+        Seller seller = new Seller(21, "Miguel", "miguel@gmail.com", LocalDate.now(), 727.0, department);
+
+        System.out.println(seller);
     }
 }
